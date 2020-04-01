455209c819a39a82fcca00f27b7bc23d631c906c
bings jaja <romich91@gmail.com>
2020-04-01 18:13:49 +0500

    some new

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,17 @@
+
 const hammenu = document.querySelector ('#hammenu');
 const hm = document.querySelector ('#hm');
+const closebtn = document.querySelector ('closebtn')
+const openbtn = document.querySelector ('openbtn')
 
 
-hammenu.addEventListener('click', function(){
+hammenu.addEventListener('click', function(e){
+  e.preventDefault();
   hm.classList.add('header__menu--active');
 }) 
+
+hammenu.addEventListener('click', function(e){
+  e.preventDefault();
+  openbtn.classList.remove('ham--active');
+  closebtn.classList.remove('ham--inactive');
+})
