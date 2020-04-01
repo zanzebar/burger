ec43c864dd4ac91a55f2e533e1c272ec2fb6be0d
bings jaja <romich91@gmail.com>
2020-04-01 16:47:29 +0500

    3d hw

---
+++ js/hamburger.js
@@ -0,0 +1,7 @@
+const hammenu = document.querySelector ('#hammenu');
+const hm = document.querySelector ('#hm');
+
+
+hammenu.addEventListener('click', function(){
+  hm.classList.add('header__menu--active');
+}) 
